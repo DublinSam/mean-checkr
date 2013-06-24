f5c4a57d4103471b75d5d5898bb608b3b03d1b9c
Sam <satiricallaughter@gmail.com>
2013-06-24 23:29:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PriceCheckkr::Application.config.secret_token = '3f3a3faebd015d7abece35b5032cffaed4d3f7d59c651ed0b6c3baf36aa091a2bc45e38789f698aba280d83c662fed636ec9df26c0a857e55bfd179387af86da'
